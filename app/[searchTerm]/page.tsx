427ecd018f8017f13c413a8cb86b3d4d44edcfcc
jarrodschilling <jarrodschilling@gmail.com>
2024-06-03 09:29:14 -0600

    project done

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,9 +1,28 @@
 import getWikiResults from "@/lib/getWikiResults"
+import Item from "@/app/[searchTerm]/components/item"
+
 type Props = {
     params: {
         searchTerm: string
     }
 }
+
+// export async function generateMetaData({ params: { searchTerm }}: Props) {
+//     const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
+//     const data = await wikiData
+//     const displayTerm = searchTerm.replaceAll('%20', '')
+
+//     if (!data?.query?.pages) {
+//         return {
+//             title: `${displayTerm} Not Found`
+//         }
+//     }
+
+//     return {
+//         title: displayTerm,
+//         description: `Search results for ${displayTerm}`
+//     }
+// }
 
 export default async function SearchResults({ params: { searchTerm }}: Props) {
     const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
@@ -14,7 +33,7 @@ export default async function SearchResults({ params: { searchTerm }}: Props) {
         <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
             {results
                 ? Object.values(results).map(result => {
-                    return <p>{JSON.stringify(result)}</p>
+                    return <Item key={result.pageid} result={result} />
                 })
                 : <h2 className="p-2 text-xl">
                     {`${searchTerm} Not Found`}
